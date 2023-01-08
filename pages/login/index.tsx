fb637a37f9cc648a5f349fbecf1b1a2010d468d2
Thitipat Na Nakorn <ihamjang@gmail.com>
2023-01-08 19:44:39 +0700

    CHORE fixed login styles

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,9 +4,11 @@ export default function LoginPage() {
     const router = useRouter();
     return <>
         <h1>Login Page</h1>
-        <form>
-            <input type="text" id="username"/>
-            <input type="password" id="password"/>
+        <form style={{border:"1px solid black",display:"flex",flexDirection: "column",gap:"1rem",padding:"1rem"}}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="please type email"/>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="please type password"/>
             <button type="button"
                 onClick={
                     () => {
